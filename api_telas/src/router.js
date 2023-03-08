6200e3a686933bca2696780d57719aafd7b24a9d
Mitzuki01 <gg.4004116@gmail.com>
2023-03-08 01:03:41 -0300

    requição para manipulação dos dados de login

--- api_telas/src/router.js
+++ api_telas/src/router.js
@@ -1,9 +1,11 @@
 import express from 'express'
 
 import user from './controller/userController.js'
+import login from './controller/loginController.js'
 
 const router = express()
 
 router.use('/user', user)
+router.use('/login',login)
 
 export default router
