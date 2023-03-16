0cb28c899b43b9c17ecde86f93a6548ac139959b
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2023-03-16 12:56:27 -0300

    Update loginController.js

--- api_telas/src/controller/loginController.js
+++ api_telas/src/controller/loginController.js
@@ -3,11 +3,20 @@ import db from '../services/userServices.js'
 
 const router = express.Router()
 
-router.post('/', async (resquest, response) => {
+router.post('/', async (req, res) => {
   const {email,senha} = resquest.body;
-  await db.loginUser(email, senha),
-  response.status(200).send({message : 'login realizado com sucesso!'})
-})
+  try{
+    const users = await db.login(email, senha);
+    if(users.length > 0){
+      res.status(200).send({message : 'login realizado com sucesso!'});
+    } else {
+      res.status(401).send({message : 'dados inseridos incorretos!'});
+    }
+  } catch(err){
+    res.status(500).send({message : `Houve um erro no banco de dados. ${err}`})
+  }
+  
+});
 
 
 export default router
