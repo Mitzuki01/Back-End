a12367c40104aa11463d07c98e6273a61480b165
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2023-03-16 12:49:08 -0300

    Update loginServices.js

--- api_telas/src/services/loginServices.js
+++ api_telas/src/services/loginServices.js
@@ -1,1 +1,13 @@
+import database from '../../repository/connection.js';
 
+async function login(email,senha) {
+  const sql = 'SELECT * FROM tbl_usuario WHERE email = ? AND senha =?';
+  const dataLogin = [email, senha];
+  
+  const conn = await database.connect();
+  const [rows] = await conn.query(sql, dataLogin);
+  
+  return rows; //[0]
+}
+
+export default {login}
