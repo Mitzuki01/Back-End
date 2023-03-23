d12dc6cc7488fcca508eef4d57c2a438c8672579
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2023-03-23 13:09:10 -0300

    Incremento de criptografia na senha {funcionando}
    foi instalado o Bcrypt, usamos a constante salt pra determina o quão seguro é a senha, dentro do hashSync definimos que a constante que recebe a nossa senha irá ser criptografada.

--- api_telas/src/controller/userController.js
+++ api_telas/src/controller/userController.js
@@ -1,12 +1,16 @@
 import express from 'express'
 import db from '../services/userServices.js'
+import bcrypt from 'bcrypt'
 
 const router = express.Router()
 
 router.post('/', async (resquest, response) => {
   const {nome_usuario,email,senha,telefone,nascimento} = resquest.body
-  console.log(senha)
+  const salt = bcrypt.genSaltSync(10);
+  const hash = bcrypt.hashSync(senha, salt);
+  console.log(hash)
   await db.createUser(nome_usuario, email, senha, telefone, nascimento)
+  //crypto funcionando, basta substituir a senha pelo hash dentro dessa await
   
   response.status(200).send({message : 'Salvo com sucesso'})
 })
