22ca923a5eca4c79808c02352e3a15172c29f380
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2023-03-29 22:12:24 -0300

    Update cadastroController.js

--- api_telas/src/controller/cadastroController.js
+++ api_telas/src/controller/cadastroController.js
@@ -6,21 +6,21 @@ const router = express.Router()
 
 router.post('/', async (request, response) => {
   const {nome_usuario, email, senha, telefone, nascimentoP} = request.body
-  const hash = md5(senha);
+  //const hash = md5(senha);
 
   await db.duplicate(email,hash)
 
   try{    
-    const userd = await db.duplicate(nome_usuario, email, hash)
+    const userd = await db.duplicate(nome_usuario, email, senha)
  
-    if(nome_usuario === '' || email === '' || hash === ''){
+    if(nome_usuario === '' || email === '' || senha === ''){
       response.status(422).send("Campo em branco")
     }
     else if(userd.length > 0){
       response.status(409).send("Usuario já cadastrado")
     }
     else{
-      await db.createUser(nome_usuario, email ,hash, telefone, nascimentoP)
+      await db.createUser(nome_usuario, email ,senha, telefone, nascimentoP)
       response.status(200).send("Cadastro realizado")
     }
     }catch{
