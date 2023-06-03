9b847d3c14fd7e524205fa39c32cec17a30b738c
AndreyJustino <100535840+AndreyJustino@users.noreply.github.com>
2023-06-03 19:34:16 -0300

    system resolutor part 6

--- src/controller/denunciaController.js
+++ src/controller/denunciaController.js
@@ -3,6 +3,8 @@ import db from '../services/denunciaService.js'
 
 import nodemailer from "nodemailer"
 
+import axios from 'axios'
+
 const router = express.Router()
 
 router.post('/', async (request, response) => {
@@ -13,6 +15,20 @@ router.post('/', async (request, response) => {
 
         // sistema para enviar email pra resolutor
         // ver como pegar a imagem, email de resolução e filtro
+
+        let config = {
+            method: 'get',
+            url: `https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longitude}&apiKey=ef172e5aac494f98ad94e03ba0d41fb8`,
+            headers: { }
+            };
+        
+        const loc = await axios(config)
+        .then(function (response) {
+        return response.data.features[0].properties.formatted;
+        })
+        .catch(function (error) {
+        return error;
+        });
 
         const email = async () => {
             let trasnporte = nodemailer.createTransport({
@@ -28,23 +44,63 @@ router.post('/', async (request, response) => {
                   }
             })
 
-            await trasnporte.sendMail({
-                from: 'Reportando <contatandosolucoes@gmail.com>', 
-                to: `andrey.justino@etec.sp.gov.br`, 
-                subject: 'Uma nova denuncia foi encontrada.', 
-                html: `<h1>Nova denuncia feita</h1>
-                <p>Os dados são:</p>
-                <ul>
-                  <li>Tipo de problema: ${tipo_problema}</li>
-                  <li>Descrição: ${desc_problema}</li>
-                  <li>Latitude: ${latitude}</li>
-                  <li>Longitude: ${longitude}</li>
-                  <li>CEP: teste </li>
-                </ul>` 
-            })
+            if (tipo_problema === "Elétrico(fio desencapado, poste caido)"){
 
-            .then(() => console.log("E-mail enviado com sucesso."))
-            .catch((err) => console.log(`Houve um erro: ${err}`))
+                await trasnporte.sendMail({
+                    from: 'Reportando <contatandosolucoes@gmail.com>', 
+                    to: `noreply.atendimentoenel@brasil.enel.com`, 
+                    subject: 'Uma nova denuncia foi encontrada.', 
+                    html: `<h1>Nova denuncia feita</h1>
+                    <p>Os dados são:</p>
+                    <ul>
+                      <li>Tipo de problema: ${tipo_problema}</li>
+                      <li>Descrição: ${desc_problema}</li>
+                      <li>Endereço: ${loc}</li>
+                    </ul>` 
+                })
+    
+                .then(() => console.log("E-mail elétrico enviado com sucesso."))
+                .catch((err) => console.log(`Houve um erro: ${err}`))
+
+            }
+            else if (tipo_problema === 'Hidraúlico(vazão de água, cano exposto)'){
+                
+                await trasnporte.sendMail({
+                    from: 'Reportando <contatandosolucoes@gmail.com>', 
+                    to: `ouvidoria@sabesp.com.br`, 
+                    subject: 'Uma nova denuncia foi encontrada.', 
+                    html: `<h1>Nova denuncia feita</h1>
+                    <p>Os dados são:</p>
+                    <ul>
+                      <li>Tipo de problema: ${tipo_problema}</li>
+                      <li>Descrição: ${desc_problema}</li>
+                      <li>Endereço: ${loc}</li>
+                    </ul>` 
+                })
+    
+                .then(() => console.log("E-mail hidraulico enviado com sucesso."))
+                .catch((err) => console.log(`Houve um erro: ${err}`))
+
+            }
+            else if (tipo_problema === "Infra(semaforo quebrado, calçada quebrada)"){
+
+                await trasnporte.sendMail({
+                    from: 'Reportando <contatandosolucoes@gmail.com>', 
+                    to: `ouvidoria@antt.gov.br`, 
+                    subject: 'Uma nova denuncia foi encontrada.', 
+                    html: `<h1>Nova denuncia feita</h1>
+                    <p>Os dados são:</p>
+                    <ul>
+                      <li>Tipo de problema: ${tipo_problema}</li>
+                      <li>Descrição: ${desc_problema}</li>
+                      <li>Endereço: ${loc}</li>
+                    </ul>` 
+                })
+    
+                .then(() => console.log("E-mail infra enviado com sucesso."))
+                .catch((err) => console.log(`Houve um erro: ${err}`))
+
+            }
 
         }
 
