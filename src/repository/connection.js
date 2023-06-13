688060fbe65b8e9cf91698ff6151ef8c988305ac
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2023-06-13 01:39:49 -0300

    Update connection.js

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -2,7 +2,7 @@ import mysql2 from 'mysql2/promise';
 
 
 async function connect() {
-    const connection = await mysql2.createConnection('mysql://lm9j4ffmagmv4vtvbtve:pscale_pw_4MBzBivF4k8d7PAHJLNExtdA9Y7qotEnMOAIFYXvfLm@aws.connect.psdb.cloud/reportando?ssl={"rejectUnauthorized":true}')
+    const connection = await mysql2.createConnection('mysql://tak140iu6rv8jepbi4oq:pscale_pw_5qlTxC8MxhT8LmAdYIzCIIlxL5GA79EB5pm1iV24Mgt@aws.connect.psdb.cloud/reportando?ssl={"rejectUnauthorized":true}')
 
     return connection;
 };
