be8f5c8ad131dbae200ab29cc1bc2f5d1e18df23
Gabriel M. Severio <83481136+Mitzuki01@users.noreply.github.com>
2023-06-20 00:03:49 -0300

    Update connection.js

--- src/repository/connection.js
+++ src/repository/connection.js
@@ -3,10 +3,10 @@ import mysql2 from 'mysql2/promise';
 
 async function connect() {
     const connection = await mysql2.createConnection({ 
-            database: 'reportando',
-            user: 'ex1ul95czxk791u30ai5',
-            host: 'aws.connect.psdb.cloud',
-            password: 'pscale_pw_MSrPExPFdLuddw83WXCJ54OdRVWkMxO95MsueJsgPz5',
+            database: 'id20758368_reportando',
+            user: 'id20758368_root',
+            host: 'localhost',
+            password: 'Etecembu@123',
             ssl : {
                rejectUnauthorized:false,
              }
